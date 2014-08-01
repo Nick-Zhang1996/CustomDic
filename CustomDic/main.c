5167b8f2fba81c1130c22948d9b1772b7b9a29ec
Nick <zhangzyhack@126.com>
2014-08-01 16:12:09 +0800

    Initial Commit

---
+++ CustomDic/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  CustomDic
+//
+//  Created by Nick zhang on 14-8-1.
+//  Copyright (c) 2014年 Nick Zhang. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
